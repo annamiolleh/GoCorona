db71d477c6d38137fa2704c052f2559234faa2c5
anna <anka301493@gmail.com>
2022-04-01 19:58:31 +0300

    Final section + minor corrections, and full adaptive version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $('.header__burger').click(function(event){
         $('.header__burger, .menu__body').toggleClass('active');
-        $('.body').toggleClass('lock');
+        $('body').toggleClass('lock');
+    });
+    $('.menu__link').click(function(event){
+        $('.header__burger, .menu__body').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
